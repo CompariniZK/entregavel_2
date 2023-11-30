58d3f4739db3f7f63bae70d7e72294fb3ce55bfa
Gabriel <gabriel.comparini.ismart@gmail.com>
2023-11-30 22:38:01 +0100

    Criei as classes e metade da estrutura. 50% feito

--- dodevSchool/js/scripts.js
+++ dodevSchool/js/scripts.js
@@ -1,238 +1,56 @@
-////////////////////////////////////////////////////////////////////////
-////////////////// FAÇA O SEU CÓDIGO AQUI \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-////////////////////////////////////////////////////////////////////////
+class Aluno{
 
-class Aluno {
+nome;
+idade;
+nota;
+
+constructor(nome, idade, nota){
+
+  this.nome = nome
+  this.idade = idade
+  this.nota = nota
+
+}
+
+}
+
+let alunosCadastrados = []
+let whilezin = true
+
+function CadastrarAluno(nome2, idade2, nota2)
+{
+while(whilezin = true){
+nome2 = prompt("Digite o nome do aluno da FEBEM:")
+idade2 = parseInt(prompt("Digite a idade(apenas números): "))
+nota2 = Number(prompt("Insira a nota do meliante: "))
+let contador = 0
+
+class FEBEM{
+
+nome1;
+idade1;
+nota1;
+
+constructor(nome1, idade1, nota1){
+
+  this.nome1 = nome1
+  this.idade1 = idade1
+  this.nota1 = nota1
+
+
+
+}
+}
+
+let fundacao = new FEBEM(nome2, idade2 , nota2)
+alunosCadastrados[contador] = fundacao
+contador++
+
+let desejo = parseInt(prompt("Deseja parar de cadastrar? (1) SIM; (2) Não"))
+if(desejo === 1){
+
+  whilezin = false
   
 }
-
-// Array
-
-
-//funções projeto
-
-function CadastrarAluno() {
-  
 }
-
-function OrdenarPorNota() {
- 
 }
-
-function OrdenarPorIdade() {
-
-}
-
-function OrdenarPorNome() {
-
-}
-
-function CalcularMedia(){
-
-}
-
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-
-function ExcluirAluno(array, nome) {
-  let index
-  let removido = false
-  array.forEach(aluno => {
-    if (aluno.Nome == nome) {
-      index = array.indexOf(aluno)
-      removido = true
-    }
-  })
-  array.splice(index, 1)
-  return removido
-}
-
-function PesquisarAluno(array, nome) {
-  let pesquisa = false
-  array.forEach(aluno => {
-    if (aluno.Nome.includes(nome)) {
-      pesquisa = true
-    }
-  })
-
-  return pesquisa
-}
-
-// Seleção de elementos
-const alunoForm = document.querySelector("#aluno-form");
-const alunoInput = document.querySelector("#aluno-input");
-const alunoInput2 = document.querySelector("#aluno-input-2");
-const alunoInput3 = document.querySelector("#aluno-input-3");
-const alunoList = document.querySelector("#aluno-list");
-const editForm = document.querySelector("#edit-form");
-const editInput = document.querySelector("#edit-input");
-const cancelEditBtn = document.querySelector("#cancel-edit-btn");
-const searchInput = document.querySelector("#search-input");
-const eraseBtn = document.querySelector("#erase-button");
-const filterBtn = document.querySelector("#filter-select");
-
-let oldInputValue;
-
-// Funções
-const saveAluno = (nome, idade, nota, done = 0, save = 1) => {
-  let objetoAluno = CadastrarAluno(nome, idade, nota, arrayAlunos)
-
-  const aluno = document.createElement("div");
-  aluno.classList.add("aluno");
-
-  const alunoNome = document.createElement("h3");
-  alunoNome.innerText = objetoAluno.Nome;
-  aluno.appendChild(alunoNome);
-
-  const alunoIdade = document.createElement("h3");
-  alunoIdade.innerText = objetoAluno.Idade;
-  aluno.appendChild(alunoIdade);
-
-  const alunoNota = document.createElement("h3");
-  alunoNota.innerText = objetoAluno.Nota;
-  aluno.appendChild(alunoNota);
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("remove-aluno");
-  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-  aluno.appendChild(deleteBtn);
-
-  // Utilizando dados da localStorage
-
-  alunoList.appendChild(aluno);
-  
-
-  const media = document.querySelector("#media");
-  media.textContent = CalcularMedia(arrayAlunos).toFixed(2)
-
-  alunoInput.value = "";
-  alunoInput2.value = "";
-  alunoInput3.value = "";
-
-};
-
-const toggleForms = () => {
-  editForm.classList.toggle("hide");
-  alunoForm.classList.toggle("hide");
-  alunoList.classList.toggle("hide");
-};
-
-const getBuscarAluno = (busca) => {
-  const alunos = document.querySelectorAll(".aluno");
-
-  let pesquisa = PesquisarAluno(arrayAlunos, busca)
-
-  if (pesquisa) {
-    alunos.forEach((aluno) => {
-      const alunoNome = aluno.querySelector("h3").innerText.toLowerCase();
-
-      aluno.style.display = "flex";
-
-      if (!alunoNome.includes(busca)) {
-        aluno.style.display = "none";
-      }
-    });
-  };
-}
-
-
-
-const filterAlunos = (filterValue) => {
-  const alunos = document.querySelectorAll(".aluno");
-
-  switch (filterValue) {
-    case "nota":
-      alunos.forEach((aluno) => {
-        aluno.remove()
-      })
-      arrayAlunos = OrdenarPorNota(arrayAlunos)
-      arrayAlunos.forEach((aluno) => saveAluno(aluno.Nome, aluno.Idade, aluno.Nota, done = 0, save = 1))
-      break;
-
-    case "idade":
-      alunos.forEach((aluno) => {
-        aluno.remove()
-      })
-      arrayAlunos = OrdenarPorIdade(arrayAlunos)
-      arrayAlunos.forEach((aluno) => saveAluno(aluno.Nome, aluno.Idade, aluno.Nota, done = 0, save = 1))
-      break;
-
-    case "nome":
-      alunos.forEach((aluno) => {
-        aluno.remove()
-      })
-      arrayAlunos = OrdenarPorNome(arrayAlunos)
-      arrayAlunos.forEach((aluno) => saveAluno(aluno.Nome, aluno.Idade, aluno.Nota, done = 0, save = 1))
-      break;
-
-    default:
-      break;
-  }
-};
-
-// Eventos
-alunoForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const inputValue = alunoInput.value;
-  const inputValue2 = alunoInput2.value;
-  const inputValue3 = alunoInput3.value;
-
-  if (inputValue && inputValue2 && inputValue3) {
-    saveAluno(inputValue, inputValue2, inputValue3);
-  }
-});
-
-document.addEventListener("click", (e) => {
-  const targetEl = e.target;
-  const parentEl = targetEl.closest("div");
-  let alunoTitle;
-
-  if (parentEl && parentEl.querySelector("h3")) {
-    alunoTitle = parentEl.querySelector("h3").innerText || "";
-  }
-
-  if (targetEl.classList.contains("remove-aluno")) {
-    alunoTitle = parentEl.querySelector("h3").innerText
-    let removido = ExcluirAluno(arrayAlunos, alunoTitle)
-    if (removido) {
-      parentEl.remove();
-
-      // Utilizando dados da localStorage
-
-    }
-
-  }
-});
-
-searchInput.addEventListener("keyup", (e) => {
-  const busca = e.target.value;
-
-  getBuscarAluno(busca);
-});
-
-eraseBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  searchInput.value = "";
-
-  searchInput.dispatchEvent(new Event("keyup"));
-});
-
-filterBtn.addEventListener("change", (e) => {
-  const filterValue = e.target.value;
-
-  filterAlunos(filterValue);
-});
-
-// Local Storage
-
-// const loadAlunos = () => {
-
-//   arrayAlunos.forEach((aluno) => {
-//     saveAluno(aluno.Nome, aluno.Idade, aluno.Nota, 0);
-//   });
-// };
-
-// loadAlunos();
